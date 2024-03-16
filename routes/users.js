c55039c2b5bdb491559d41992c37c2d29b0e39d4
sricheekati <cheekatisrilakshmi04@gmail.com>
2024-03-15 22:39:11 -0500

    modified users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var product=1;
+var factor=1;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product *= factor;
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
