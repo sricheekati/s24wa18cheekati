b44c17238b4c5a13bc9dd6b1534582b2808d25fc
sricheekati <cheekatisrilakshmi04@gmail.com>
2024-03-15 22:12:24 -0500

    new Endpoint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # s24wa18cheekati
+
+Render server hosted at link : https://s24wa18cheekati.onrender.com 
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -15,12 +16,14 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
