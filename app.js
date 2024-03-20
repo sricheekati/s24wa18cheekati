8fd5f704c4eae1a4de564b0652be2cab8e504692
sricheekati <cheekatisrilakshmi04@gmail.com>
2024-03-20 11:06:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use('/mydata', mydataRouter);
-app.use('/computation', computationRouter);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
